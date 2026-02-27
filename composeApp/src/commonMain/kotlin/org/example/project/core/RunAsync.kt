a6ca0750990d97e1fbc1f300d419c5b3cf67e3aa
Aman <kemalov2004@mail.ru>
2026-02-27 21:25:08 +0500

    Add RunAsync and OnboardingRepository implementations

--- composeApp/src/commonMain/kotlin/org/example/project/core/RunAsync.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/core/RunAsync.kt
@@ -1,7 +1,13 @@
 package org.example.project.core
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.IO
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 interface RunAsync {
     fun <T : Any> runAsync(
@@ -15,4 +21,26 @@ interface RunAsync {
         flow: Flow<T>,
         onEach: (T) -> Unit,
     )
+    class Base : RunAsync {
+        override fun <T : Any> runAsync(
+            scope: CoroutineScope,
+            background: suspend () -> T,
+            ui: (T) -> Unit
+        ) {
+            scope.launch(Dispatchers.IO) {
+                val data = background.invoke()
+                withContext(Dispatchers.Main) {
+                    ui.invoke(data)
+                }
+            }
+        }
+
+        override fun <T : Any> runFlow(
+            scope: CoroutineScope,
+            flow: Flow<T>,
+            onEach: (T) -> Unit
+        ) {
+            flow.onEach(onEach).launchIn(scope)
+        }
+    }
 }
