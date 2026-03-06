add0f1d9dd388e1455520e175f15a827ad26dda6
Aman <kemalov2004@mail.ru>
2026-03-06 23:16:38 +0500

    rename AuthRepositoryTest to LoginRepositoryTest and add constants

--- composeApp/src/commonTest/kotlin/org/example/project/LoginRepositoryTest.kt
+++ composeApp/src/commonTest/kotlin/org/example/project/LoginRepositoryTest.kt
@@ -8,40 +8,44 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
-class AuthRepositoryTest {
-    private lateinit var authRepository: LoginRepository
+class LoginRepositoryTest {
+    private lateinit var loginRepository: LoginRepository
     private lateinit var authWrapper: FakeAuthWrapper
 
     @BeforeTest
     fun setUp() {
         authWrapper = FakeAuthWrapper()
-        authRepository = LoginRepositoryImpl(authWrapper = authWrapper)
+        loginRepository = LoginRepositoryImpl(authWrapper = authWrapper)
     }
 
     @Test
     fun success() = runBlocking {
-        val actualResult = authRepository.userToken()
-        val expectedResult = Result.success("fakeToken")
+        val actualResult = loginRepository.userToken()
+        val expectedResult = Result.success(FAKE_TOKEN)
         assertEquals(expectedResult, actualResult)
 
         val currentToken = TokenStorage.token
         assertEquals("", currentToken)
 
-        authRepository.saveUserToken("fakeToken")
+        loginRepository.saveUserToken(FAKE_TOKEN)
         val actualSavedToken = TokenStorage.token
-        assertEquals("fakeToken", actualSavedToken)
+        assertEquals(FAKE_TOKEN, actualSavedToken)
     }
 
     @Test
     fun failure() = runBlocking {
-        val expectedException = IllegalStateException("fake error message")
+        val expectedException = IllegalStateException(FAKE_ERROR_MESSAGE)
         authWrapper.setException(expectedException)
 
-        val actualResult = authRepository.userToken()
+        val actualResult = loginRepository.userToken()
         assertTrue(actualResult.isFailure)
         val error = actualResult.exceptionOrNull()!!
         assertEquals(expectedException.message, error.message)
     }
 
+    companion object {
+        private const val FAKE_TOKEN = "fakeToken"
+        private const val FAKE_ERROR_MESSAGE = "fake error message"
+    }
 }
 
