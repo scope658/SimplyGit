0d51900e9b3d632e79c348cfb1f4d2aa13eb0a66
Aman <kemalov2004@mail.ru>
2026-03-08 19:44:58 +0500

    update main di

--- composeApp/src/commonMain/kotlin/org/example/project/main/di/MainModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/main/di/MainModule.kt
@@ -1,17 +1,20 @@
 package org.example.project.main.di
 
-import org.example.project.main.data.ListRepositoryImpl
-import org.example.project.main.domain.ListRepository
+import org.example.project.main.data.MainRepositoryImpl
+import org.example.project.main.data.cloud.GithubApi
+import org.example.project.main.domain.MainRepository
 import org.example.project.main.presentation.MainViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 
 val mainModule = module {
-    single<ListRepository> { ListRepositoryImpl() }
+    single<GithubApi> { GithubApi.Base(get()) }
+    single<MainRepository> { MainRepositoryImpl(get()) }
     viewModel {
         MainViewModel(
             runAsync = get(),
             repository = get(),
+            savedStateHandle = get(),
         )
     }
 }
