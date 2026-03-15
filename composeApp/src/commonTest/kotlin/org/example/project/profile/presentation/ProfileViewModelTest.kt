7f5eaf4df4568c04fddbe820b92a20e3db53c8d3
Aman <kemalov2004@mail.ru>
2026-03-15 22:45:01 +0500

    add logout to  ProfileViewModelTest and to impl

--- composeApp/src/commonTest/kotlin/org/example/project/profile/presentation/ProfileViewModelTest.kt
+++ composeApp/src/commonTest/kotlin/org/example/project/profile/presentation/ProfileViewModelTest.kt
@@ -1,25 +1,30 @@
-package org.example.project
+package org.example.project.profile.presentation
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import org.example.project.core.ControlledFakeRunAsync
+import org.example.project.profile.domain.Profile
 import org.example.project.profile.domain.ProfileRepository
-import org.example.project.profile.domain.UserProfile
-import org.example.project.profile.presentation.ProfileUiState
-import org.example.project.profile.presentation.ProfileViewModel
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class ProfileViewModelTest {
 
     @Test
-    fun `failure load profile then success`() {
+    fun `failure load profile then success`() = runBlocking {
         val profileRepository = FakeProfileRepository()
         profileRepository.isFailure(true)
         val runAsync = ControlledFakeRunAsync()
+
         val profileViewModel = ProfileViewModel(
             runAsync = runAsync,
             profileRepository = profileRepository
         )
+
         val profileUiState: StateFlow<ProfileUiState> = profileViewModel.profileUiState
         assertEquals(ProfileUiState.Loading, profileUiState.value)
 
@@ -35,7 +40,21 @@ class ProfileViewModelTest {
         runAsync.invokeUi()
 
         assertEquals(successUiResult, profileUiState.value)
+
+        val profileEvent: SharedFlow<ProfileEvent> = profileViewModel.profileEvent
+        val job: Job = launch(Dispatchers.Unconfined) {
+            profileEvent
+                .collect {
+                    assertEquals(ProfileEvent.Logout, it)
+                }
+        }
+
+        profileViewModel.logout()
+        profileRepository.checkLogoutIsCalled(1)
+        runAsync.invokeUi()
+        job.cancel()
     }
+
 }
 
 private val successUiResult = ProfileUiState.Success(
@@ -50,18 +69,19 @@ private val failureUiResult = ProfileUiState.Failure(
     message = "fake message"
 )
 
-private val mockedSuccessResult = UserProfile(
+private val mockedSuccessResult = Profile(
     avatar = "fakeImageUrl",
     userName = "scope",
     bio = "fakeBio",
-    repoCount = "12",
-    subscribersCount = "33"
+    repoCount = 12,
+    subscribersCount = 33,
 )
 
 private class FakeProfileRepository : ProfileRepository {
 
     private var isFailure = false
-    override suspend fun userProfile(): Result<UserProfile> {
+    private var logoutCalledTimes = 0
+    override suspend fun userProfile(): Result<Profile> {
         return if (isFailure) {
             Result.failure(IllegalStateException("fake message"))
         } else {
@@ -69,7 +89,15 @@ private class FakeProfileRepository : ProfileRepository {
         }
     }
 
+    override suspend fun logout() {
+        logoutCalledTimes++
+    }
+
     fun isFailure(isFailure: Boolean) {
         this.isFailure = isFailure
+    }
+
+    fun checkLogoutIsCalled(expectedTimes: Int) {
+        assertEquals(expectedTimes, logoutCalledTimes)
     }
 }
