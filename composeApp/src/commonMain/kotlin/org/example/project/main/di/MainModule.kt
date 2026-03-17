1f25c9f1b72e9fb2798ef96924430563f5ba2cf5
Aman <kemalov2004@mail.ru>
2026-03-17 10:00:00 +0500

    make PagedResult.Mapper generic

--- composeApp/src/commonMain/kotlin/org/example/project/main/di/MainModule.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/main/di/MainModule.kt
@@ -1,10 +1,13 @@
 package org.example.project.main.di
 
 import org.example.project.core.cache.DataStoreManager
+import org.example.project.core.cache.db.AppDatabase
 import org.example.project.main.data.MainRepositoryImpl
+import org.example.project.main.data.cache.UserRepoDao
 import org.example.project.main.data.cloud.GithubApi
 import org.example.project.main.domain.GetPagedReposUseCase
 import org.example.project.main.domain.HandleMainRequest
+import org.example.project.main.domain.HandleUserRepoRequest
 import org.example.project.main.domain.MainRepository
 import org.example.project.main.domain.PagedResult
 import org.example.project.main.presentation.MainUiMapper
@@ -16,19 +19,21 @@ val mainModule = module {
     single<UserRepoDao> { get<AppDatabase>().userRepoDao() }
     single<GithubApi> { GithubApiImpl(get()) }
     single<DataStoreManager.ReadToken> { DataStoreManager.Base(dataStore = get()) }
+    single<GithubApi> { GithubApi.Base(httpClient = get()) }
     single<MainRepository> {
         MainRepositoryImpl(
             githubApi = get(), dataStoreManager = get(),
             dao = get()
         )
     }
-    single<PagedResult.Mapper> { MainUiMapper() }
-    single<HandleMainRequest> { HandleMainRequest() }
+    single<PagedResult.Mapper<MainUiState>> { MainUiMapper() }
+    single<HandleMainRequest> { HandleMainRequest.Base() }
     factory<HandleUserRepoRequest> { HandleUserRepoRequest.Base() }
     single<GetPagedReposUseCase> {
         GetPagedReposUseCaseImpl(
             handleMainRequest = get(),
-            repository = get()
+            repository = get(),
+            handleUserRepoRequest = get()
         )
     }
     viewModel {
