b1358ede648a00774f8f32d7f45f68b813a6a0f7
Aman <kemalov2004@mail.ru>
2026-04-06 13:17:34 +0500

    add detailsUseCaseTest and detailsUseCase

---
+++ composeApp/src/commonMain/kotlin/org/example/project/details/domain/RepoDetailsUseCaseImpl.kt
@@ -0,0 +1,30 @@
+package org.example.project.details.domain
+
+class RepoDetailsUseCaseImpl(
+    private val handleDetailsRequest: HandleDetailsRequest,
+    private val detailsRepository: DetailsRepository
+) : RepoDetailsUseCase {
+    override suspend fun repoDetails(
+        repoOwner: String,
+        repoName: String
+    ): CombinedDetailsResult {
+        return handleDetailsRequest.handle(readme = {
+            detailsRepository.readme(repoOwner, repoName)
+        }, repoDetails = {
+            detailsRepository.repoDetails(repoOwner, repoName)
+        }
+        )
+    }
+
+    override suspend fun refreshRepoDetails(
+        repoOwner: String,
+        repoName: String
+    ): CombinedDetailsResult {
+        return handleDetailsRequest.handle(readme = {
+            detailsRepository.refreshReadme(repoOwner, repoName)
+        }, repoDetails = {
+            detailsRepository.refreshDetails(repoOwner, repoName)
+        }
+        )
+    }
+}
